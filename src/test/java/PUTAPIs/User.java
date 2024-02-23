f36608d94e584b2e996dbd91f6dacccba2fd1ff4
Rithica20 <rithica1320@gmail.com>
2024-02-23 16:56:05 +0530

    delete and put api

---
+++ src/test/java/PUTAPIs/User.java
@@ -0,0 +1,20 @@
+package PUTAPIs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+//{
+//  "name" : "sahil kumar",
+//  "email" : "sahil126456022657@gmail.com",
+//  "gender" : "male",
+//  "status" : "active"
+//}
+        private String name;
+        private String email;
+        private String gender;
+        private String status;
+}
